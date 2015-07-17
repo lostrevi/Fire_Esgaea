481228242186afa4b5e758de0c348f88e2a1d5a8
Derek Slager <lostrevi@gmail.com>
2015-07-16 21:40:34 -0500

    derp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+	
+	Fire_Esgaea 0.0.0-1
+	
+			By:
+	   	  Revi09
+	   	  
+	   	  This is a shitty game i'm going to try to make. neat.
+	
+*/
+
+#include <SDL.h>
+#include <SDL_Image.h>
+#include <SDL_ttf.h>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
